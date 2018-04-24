ed9c91f29703b3437212a00cf7baf7a8583cf51a
jrah <jjames.home@gmail.com>
2018-04-24 14:54:53 +0100

    Add image optimisation

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 const postcss = require('gulp-postcss');
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
 const autoprefixer = require('autoprefixer');
 const purgecss = require('postcss-purgecss');
 const clean = require('postcss-clean');
 
-gulp.task('build', () => {
+gulp.task('css', () => {
   const plugins = [
     autoprefixer({ browsers: ['last 2 version'] }),
     purgecss({
@@ -16,4 +17,15 @@ gulp.task('build', () => {
   return gulp.src('_site/assets/main.css')
     .pipe(postcss(plugins))
     .pipe(gulp.dest('_site/assets/'));
+});
+
+
+gulp.task('images', () => {
+  gulp.src('assets/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('_site/assets/images'));
+});
+
+gulp.task('build', () => {
+  gulp.run('css', 'images');
 });
